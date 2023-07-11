29db96223a1ceb183fad0429b5cd3657cd944857
Poltavskay <poltavskay9@gmail.com>
2023-07-11 13:38:42 +0300

    Added axios integration so it shows the weather of a city

--- src/App.js
+++ src/App.js
@@ -1,22 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import Weather from "./Weather";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
+        <h1>Hello from React</h1>
+        <br />
+        <a href="https://shecodes.io/" target="_blank" rel="noreferrer">
+          SheCodes
         </a>
+        <img src="test.png" alt="SheCodes Logo" />
+        <Weather city="Tokyo"/>
       </header>
     </div>
   );
